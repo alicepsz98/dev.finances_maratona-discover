015d0e944f4b4dcf59ba293bc7d615370c32850e
Alice <alicepsouzaba1998@gmail.com>
2021-02-07 12:31:38 -0300

    transaction model

--- js/script.js
+++ js/script.js
@@ -12,3 +12,37 @@ const Modal = {
       .remove('active')
   }
 }
+
+const Transaction = {
+  incomes() {
+
+  },
+  expanses() {
+
+  },
+  total() {
+
+  }
+}
+
+const transactions = [
+  {
+    id: 1,
+    description: 'Luz',
+    amount: -50000,
+    date: '23/01/2021'
+  },
+  {
+    id: 2,
+    description: 'Gás',
+    amount: -7000,
+    date: '23/01/2021'
+  },
+  {
+    id: 3,
+    description: 'Internet',
+    amount: -10000,
+    date: '23/01/2021'
+  },
+]
+
